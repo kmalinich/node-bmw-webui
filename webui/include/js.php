f788a3e8b2b0be9fa936ae45394b7866b7aaa51c
kdm <kennygprs@gmail.com>
2017-10-02 18:05:38 -0400

    Update

--- webui/include/js.php
+++ webui/include/js.php
@@ -4,6 +4,7 @@
 <script src="/js/moment.js"></script>
 <script src="/js/moment-tz.js"></script>
 -->
+<script src="js/popper.js"></script>
 <script src="js/bootstrap.js"></script>
 <script src="js/material.js"></script>
 <script src="js/nouislider.js"></script>
