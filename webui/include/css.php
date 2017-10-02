e7ca1eb4a0031b77315dadbc7bac1afe965c251b
kdm <kennygprs@gmail.com>
2017-10-02 19:17:13 -0400

    Update

--- webui/include/css.php
+++ webui/include/css.php
@@ -1,5 +1,5 @@
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700">
 <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="css/bootstrap-material-design.css"/>
+<link rel="stylesheet" href="css/material.css"/>
 <link rel="stylesheet" href="css/nouislider.css"/>
 <link rel="stylesheet" href="css/node-bmw.css"/>
