2b593fd0f7d51f127bc9be2ddc20bdc74ac71cf5
kdm <kennygprs@gmail.com>
2017-10-02 19:19:40 -0400

    Update

--- webui/include/navbar.php
+++ webui/include/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 	<a class="navbar-brand" href="/">node-bmw</a>
 
 	<button class="navbar-toggler" type="button" data-toggle="colla:wpse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
