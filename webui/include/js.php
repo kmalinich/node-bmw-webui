b81aef999c76cb3b874e27c56d4c7de69c07d66e
kdm <kennygprs@gmail.com>
2017-10-13 19:27:02 -0400

    Update

--- webui/include/js.php
+++ webui/include/js.php
@@ -1,18 +1,18 @@
-<script async src="js/socket.io.js"></script>
-<script async src="js/jquery.js"></script>
+<script src="js/socket.io.js"></script>
+<script src="js/jquery.js"></script>
 <!--
-<script async src="/js/moment.js"></script>
-<script async src="/js/moment-tz.js"></script>
+<script src="/js/moment.js"></script>
+<script src="/js/moment-tz.js"></script>
 -->
-<script async src="js/popper.js"></script>
-<script async src="js/material.js"></script>
-<script async src="js/nouislider.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/material.js"></script>
+<script src="js/nouislider.js"></script>
 
 <!--
-<script async src="/js/d3.js"></script>
+<script src="/js/d3.js"></script>
 -->
-<script async src="https://d3js.org/d3.v3.min.js"></script>
+<script src="https://d3js.org/d3.v3.min.js"></script>
 
-<script async src="js/gauge.js"></script>
-<script async src="js/websocket.js"></script>
-<script async src="js/node-bmw.js"></script>
+<script src="js/gauge.js"></script>
+<script src="js/websocket.js"></script>
+<script src="js/node-bmw.js"></script>
