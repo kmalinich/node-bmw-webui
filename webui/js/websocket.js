1d289c15bc423705480963d30b037d1648ba4ac0
kdm <kennygprs@gmail.com>
2017-10-13 19:56:07 -0400

    Update

--- webui/js/websocket.js
+++ webui/js/websocket.js
@@ -134,7 +134,7 @@ function init_dash() {
 function on_status_tx(data) {
 	if (window.socket_debug === true) console.log(data);
 
-	let path_hyphen = data.key.full.replace('.', '-');
+	let path_hyphen = data.key.full.replace(/\./, '-');
 	console.log('path-dot : \'%s\', path-hyphen: \'%s\'', data.key.full, path_hyphen);
 
 	switch (data.key.full) {
