1c29970c63e04f575dc707e1dc26612ac1ad2e32
kdm <kennygprs@gmail.com>
2017-10-24 19:02:16 -0400

    Update

--- webui/include/js.php
+++ webui/include/js.php
@@ -1,14 +1,14 @@
 <script src="js/socket.io.js"></script>
 <script src="js/jquery.js"></script>
 <!--
-<script src="/js/moment.js"></script>
-<script src="/js/moment-tz.js"></script>
+<script src="js/moment.js"></script>
+<script src="js/moment-tz.js"></script>
 -->
 <script src="js/popper.js"></script>
 <script src="js/material.js"></script>
 <script src="js/nouislider.js"></script>
 
-<script src="/js/d3.js"></script>
+<script src="js/d3.js"></script>
 
 <script src="js/gauge.js"></script>
 <script src="js/websocket.js"></script>
